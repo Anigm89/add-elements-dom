6b77ada0c34fce201ad42384b93bf25c92f9aa5b
Aniana <anigm89@gmail.com>
2023-11-07 20:54:34 +0100

    actualizado else

--- js/script.js
+++ js/script.js
@@ -1,44 +1,18 @@
- // Aquí tu código
 const pulsar= document.getElementById('agregar');
 
-pulsar.addEventListener('click', function(){
+pulsar.addEventListener('click', function()
+{
     const elemento= prompt('Introduce un elemento a la lista');
-    //return elemento;
-    if(elemento != ''){
-        const listado= document.getElementById('lista');
+    if(elemento != '')
+    {
+        const listado= document.getElementById('lista');       
         const elementlist = document.createElement('li'); 
         listado.appendChild(elementlist);
-        //elementlist.innerHTML();
         elementlist.insertAdjacentHTML(
-            'afterbegin',
-            `${elemento} `
-          );    
+            'afterbegin',  `${elemento} ` );    
     }
-    else{
-     
-      const listado= document.getElementById('lista');
-      const elementlist = document.createElement('li'); 
-      listado.appendChild(elementlist);
-      elementlist.insertAdjacentHTML(
-          'afterbegin',
-          'No ha introducido un elemento '
-        );    
+    else
+    {
+     alert('No has introducido nada'); 
     }
 } );
-
-       
-
-
-
-
-/*
-
- const nombre = prompt('Introduce tu nombre');
-
-const pulse = document.getElementById('pulse');
-const texto = document.getElementById('addtexto');
-
-pulse.addEventListener('click', function () {
-  texto.innerHTML = `Hola ${nombre}, recuerda que JS mola mucho`;
-});
-*/
