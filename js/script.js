a4bd00f8b74fd47fa095b6efa167c17c936c5644
Aniana <anigm89@gmail.com>
2023-11-07 13:09:20 +0100

    ejercicio terminado

--- js/script.js
+++ js/script.js
@@ -1,1 +1,34 @@
  // Aquí tu código
+const pulsar= document.getElementById('agregar');
+
+pulsar.addEventListener('click', function(){
+    const elemento= prompt('Introduce un elemento a la lista');
+    //return elemento;
+    if(elemento != ''){
+        const listado= document.getElementById('lista');console.log(listado)
+        const elementlist = document.createElement('li'); console.log(elementlist)
+        listado.appendChild(elementlist);
+        //elementlist.innerHTML();
+        elementlist.insertAdjacentHTML(
+            'afterbegin',
+            `${elemento} `
+          );    
+    }
+} );
+
+       
+
+
+
+
+/*
+
+ const nombre = prompt('Introduce tu nombre');
+
+const pulse = document.getElementById('pulse');
+const texto = document.getElementById('addtexto');
+
+pulse.addEventListener('click', function () {
+  texto.innerHTML = `Hola ${nombre}, recuerda que JS mola mucho`;
+});
+*/
